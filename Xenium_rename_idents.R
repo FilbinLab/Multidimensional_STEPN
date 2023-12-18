75a788105506ee186e8b5a02e8a180da3e899da0
sara.dani94@gmail.com <sara.dani94@gmail.com>
2023-12-18 11:04:18 -0500

    correct script

--- Xenium_rename_idents.R
+++ Xenium_rename_idents.R
@@ -17,6 +17,20 @@ if (!dir.exists(plot_dir)){dir.create(plot_dir, recursive = T)}
 data_dir <- file.path(base_dir, 'analysis/data')
 if (!dir.exists(data_dir)){dir.create(data_dir, recursive = T)}
 
+## Create directories to store individual outputs
+if (!dir.exists(file.path(plot_dir, 'individual'))){dir.create(file.path(plot_dir, 'individual'), recursive = T)}
+
+samples <- c('0010540-Region_1', '0010540-Region_2', '0010540-Region_3', '0010540-Region_4', 
+             '0010553-Region_1', '0010553-Region_2', '0010553-Region_3', '0010553-Region_4')
+names(samples) <- c('0010540-Region_1', '0010540-Region_2', '0010540-Region_3', '0010540-Region_4', 
+             '0010553-Region_1', '0010553-Region_2', '0010553-Region_3', '0010553-Region_4')
+
+for (i in seq_along(samples)) {
+  if (!dir.exists(file.path(plot_dir, paste0('individual/', names(samples)[i])))){dir.create(file.path(plot_dir, paste0('individual/', names(samples)[i])), recursive = T)}
+  
+}
+    
+
 
 # Load gene list of Xenium panel -------------------------------------
 Xenium_panel <- read_excel(file.path(base_dir, 'Xenium_panel.xlsx'))
@@ -52,32 +66,51 @@ gene_list$Glio_Angiogenesis <- NULL
 gene_list$`NPC-like-1` <- NULL
 gene_list$`NPC-like-2` <- NULL
 
-file_names <- c('0010540-Region_1',
-                '0010540-Region_2',
-                '0010540-Region_3',
-                '0010540-Region_4',
-                '0010553-Region_1',
-                '0010553-Region_2',
-                '0010553-Region_3',
-                '0010553-Region_4')
+# save Xenium gene list (for tumor subpopulations)
+saveRDS(gene_list, file.path(data_dir, 'Xenium_tumor_gene_list.rds'))
+
+
 
 # Load Xenium output 0010540-Region_1 (processed by Carlos) -------------------------------------
 
 # Read data
 data <- qread(file.path(base_dir, 'raw_data/0010540-Region_1.qs'))
 
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010540-Region_1/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
+
 # Change name identities
 Idents(data) <- data$SCT_snn_res.0.8
-data <- RenameIdents(data, '0' = 'NPC-like', '1' = 'Ependymal-like', '2' = 'Ependymal-like', '3' = 'NPC-like', '4' = 'Unassigned', '5' = 'Unassigned',
-                     '6' = 'NPC-like', '7' = 'Ependymal-like', '8' = 'Neuroepithelial-like', '9' = 'Immune', '10' = 'Unassigned', '11' = 'Mesenchymal',
-                     '12' = 'Unassigned', '13' = 'Neuroepithelial-like')
+data <- RenameIdents(data, 
+                     '0' = 'NPC-like', 
+                     '1' = 'Ependymal-like', 
+                     '2' = 'Ependymal-like', 
+                     '3' = 'NPC-like', 
+                     '4' = 'Unassigned', 
+                     '5' = 'Unassigned',
+                     '6' = 'NPC-like', 
+                     '7' = 'Ependymal-like', 
+                     '8' = 'Neuroepithelial-like', 
+                     '9' = 'Immune', 
+                     '10' = 'Unassigned', 
+                     '11' = 'Mesenchymal',
+                     '12' = 'NPC-like', 
+                     '13' = 'Neuroepithelial-like')
 
 data[["group"]] <- Idents(data)
 
 # DotPlot markers
 DotPlot(data, cols = c('lightgrey', 'red'),features = gene_list, assay = 'SCT', scale = TRUE) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 8))
-ggsave(file.path(plot_dir, "1_DotPlot_0010540-Region_1_3EP8.pdf"), width=13, height=4)
+ggsave(file.path(plot_dir, "/1_DotPlot_0010540-Region_1_3EP8.pdf"), width=13, height=4)
 
 # Export metadata with cell names and new annotation
 cell_id <- data@meta.data %>% select(group)
@@ -93,11 +126,35 @@ write_csv(cell_id, file.path(data_dir, "0010540-Region_1_3EP8_cell_id.csv"))
 # Read data
 data <- qread(file.path(base_dir, 'raw_data/0010540-Region_2.qs'))
 
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010540-Region_2/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
 # Change name identities
 Idents(data) <- data$SCT_snn_res.0.9
-data <- RenameIdents(data, '0' = 'NPC-like', '1' = 'Unassigned', '2' = 'Ependymal-like', '3' = 'Ependymal-like', '4' = 'Radial glia-like', '5' = 'NPC-like',
-                     '6' = 'Unassigned', '7' = 'NPC-like', '8' = 'Ependymal-like', '9' = 'Neuroepithelial-like', '10' = 'Mesenchymal', '11' = 'Immune',
-                     '12' = 'Unassigned', '13' = 'Neuroepithelial-like', '14' = 'Neuroepithelial-like', '15' = 'Immune')
+data <- RenameIdents(data, 
+                     '0' = 'NPC-like', 
+                     '1' = 'NPC-like', 
+                     '2' = 'Ependymal-like', 
+                     '3' = 'Ependymal-like', 
+                     '4' = 'Radial glia-like', 
+                     '5' = 'NPC-like',
+                     '6' = 'NPC-like', 
+                     '7' = 'NPC-like', 
+                     '8' = 'Ependymal-like', 
+                     '9' = 'Neuroepithelial-like', 
+                     '10' = 'Mesenchymal', 
+                     '11' = 'Immune',
+                     '12' = 'NPC-like', 
+                     '13' = 'Neuroepithelial-like', 
+                     '14' = 'Neuroepithelial-like', 
+                     '15' = 'Immune')
 
 data[["group"]] <- Idents(data)
 
@@ -120,25 +177,36 @@ write_csv(cell_id, file.path(data_dir, "0010540-Region_2_3EP8_cell_id.csv"))
 # Read data
 data <- qread(file.path(base_dir, 'raw_data/0010540-Region_3.qs'))
 
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010540-Region_3/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
+
 # Change name identities
 Idents(data) <- data$SCT_snn_res.1
 data <- RenameIdents(data, 
-                     '0' = 'NPC-like/Radial-glia', 
+                     '0' = 'NPC-like', 
                      '1' = 'Ependymal-like', 
-                     '2' = 'Normal neurons', 
+                     '2' = 'Mesenchymal', 
                      '3' = 'Tumor-c24', 
                      '4' = 'Tumor-c24', 
                      '5' = 'Ependymal-like',
-                     '6' = 'NPC-like/Radial-glia', 
+                     '6' = 'NPC-like', 
                      '7' = 'Normal neurons', 
-                     '8' = 'NPC-like/Radial-glia', 
+                     '8' = 'NPC-like', 
                      '9' = 'Ependymal-like', 
                      '10' = 'Neuroepithelial-like', 
                      '11' = 'Immune',
                      '12' = 'Unassigned', 
                      '13' = 'Neuroepithelial-like', 
                      '14' = 'Unassigned', 
-                     '15' = 'Unassigned',
+                     '15' = 'Mesenchymal',
                      '16' = 'Tumor-c24')
 
 data[["group"]] <- Idents(data)
@@ -162,13 +230,24 @@ write_csv(cell_id, file.path(data_dir, "0010540-Region_3_BT1743_cell_id.csv"))
 # Read data
 data <- qread(file.path(base_dir, 'raw_data/0010540-Region_4.qs'))
 
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010540-Region_4/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
+
 # Change name identities
 Idents(data) <- data$SCT_snn_res.0.3
 data <- RenameIdents(data, 
                      '0' = 'Ependymal-like', 
                      '1' = 'Ependymal-like', 
                      '2' = 'NPC-like', 
-                     '3' = 'Unassigned', 
+                     '3' = 'Ependymal-like', 
                      '4' = 'Immune', 
                      '5' = 'Endothelial',
                      '6' = 'Mesenchymal', 
@@ -199,23 +278,34 @@ write_csv(cell_id, file.path(data_dir, "0010540-Region_4_7EP1_cell_id.csv"))
 # Read data
 data <- qread(file.path(base_dir, 'raw_data/0010553-Region_1.qs'))
 
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010553-Region_1/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
+
 # Change name identities
 Idents(data) <- data$SCT_snn_res.0.7
 data <- RenameIdents(data, 
                      '0' = 'Immune', 
                      '1' = 'Ependymal-like', 
                      '2' = 'Ependymal-like', 
-                     '3' = 'Normal neurons', 
+                     '3' = 'NPC-like', 
                      '4' = 'Ependymal-like', 
                      '5' = 'Neuroepithelial-like',
                      '6' = 'Immune', 
-                     '7' = 'Normal neurons', 
+                     '7' = 'NPC-like', 
                      '8' = 'Unassigned', 
                      '9' = 'Endothelial', 
                      '10' = 'Immune',
                      '11' = 'Immune',
                      '12' = 'Immune',
-                     '13' = 'Normal neurons')
+                     '13' = 'NPC-like')
 
 data[["group"]] <- Idents(data)
 
@@ -240,6 +330,16 @@ write_csv(cell_id, file.path(data_dir, "0010553-Region_1_11EP22_cell_id.csv"))
 # Read data
 data <- qread(file.path(base_dir, 'raw_data/0010553-Region_2.qs'))
 
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010553-Region_2/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
 # Change name identities
 Idents(data) <- data$SCT_snn_res.0.7
 data <- RenameIdents(data, 
@@ -248,16 +348,16 @@ data <- RenameIdents(data,
                      '2' = 'Ependymal-like', 
                      '3' = 'Ependymal-like', 
                      '4' = 'Neuroepithelial-like', 
-                     '5' = 'Neurons',
-                     '6' = 'Neurons', 
+                     '5' = 'NPC-like',
+                     '6' = 'NPC-like', 
                      '7' = 'Immune', 
                      '8' = 'Unassigned', 
                      '9' = 'Immune', 
                      '10' = 'Endothelial',
                      '11' = 'Unassigned',
-                     '12' = 'Ependymal',
+                     '12' = 'Ependymal-like',
                      '13' = 'Immune',
-                     '14' = 'Unassigned')
+                     '14' = 'NPC-like')
 
 data[["group"]] <- Idents(data)
 
@@ -272,6 +372,109 @@ cell_id <-  cell_id %>%
   rownames_to_column(var = "cell_id")
 
 write_csv(cell_id, file.path(data_dir, "0010553-Region_2_11EP22_cell_id.csv"))
+
+
+
+
+
+
+# Load Xenium output 0010553-Region_3 (processed by Carlos) -------------------------------------
+
+# Read data
+data <- qread(file.path(base_dir, 'raw_data/0010553-Region_3.qs'))
+
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010553-Region_3/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
+# Change name identities
+Idents(data) <- data$SCT_snn_res.0.7
+data <- RenameIdents(data, 
+                     '0' = 'Unassigned', 
+                     '1' = 'NPC-like', 
+                     '2' = 'Unassigned', 
+                     '3' = 'Ependymal-like', 
+                     '4' = 'Neurons', 
+                     '5' = 'Mesenchymal',
+                     '6' = 'Unassigned', 
+                     '7' = 'Ependymal-like', 
+                     '8' = 'Unassigned', 
+                     '9' = 'Neuroepithelial-like', 
+                     '10' = 'Neuroepithelial-like',
+                     '11' = 'Immune',
+                     '12' = 'NPC-like',
+                     '13' = 'Neuroepithelial-like')
+
+data[["group"]] <- Idents(data)
+
+# DotPlot markers
+DotPlot(data, cols = c('lightgrey', 'red'),features = gene_list, assay = 'SCT', scale = TRUE) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 8))
+ggsave(file.path(plot_dir, "1_DotPlot_0010553-Region_3_11EP22.pdf"), width=13, height=4)
+
+# Export metadata with cell names and new annotation
+cell_id <- data@meta.data %>% select(group)
+cell_id <-  cell_id %>%
+  rownames_to_column(var = "cell_id")
+
+write_csv(cell_id, file.path(data_dir, "0010553-Region_3_11EP22_cell_id.csv"))
+
+
+
+
+
+
+# Load Xenium output 0010553-Region_4 (processed by Carlos) -------------------------------------
+
+# Read data
+data <- qread(file.path(base_dir, 'raw_data/0010553-Region_4.qs'))
+
+# DotPlot markers for different resolutions
+nres <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+for (res in nres) {
+  Idents(data) <- paste0("SCT_snn_res.", res)
+  # DotPlot markers
+  DotPlot(data, cols = c('lightgrey', 'red'), features = gene_list, assay = 'SCT', scale = TRUE) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 6))
+  ggsave(file.path(plot_dir, paste0("individual/0010553-Region_4/1_DotPlot_res", res ,".pdf")), width=14, height=5)
+}
+
+# Change name identities
+Idents(data) <- data$SCT_snn_res.0.3
+data <- RenameIdents(data, 
+                     '0' = 'NPC-like', 
+                     '1' = 'Ependymal-like', 
+                     '2' = 'Ependymal-like', 
+                     '3' = 'Endothelial', 
+                     '4' = 'Immune', 
+                     '5' = 'Neuroepithelial-like',
+                     '6' = 'NPC-like', 
+                     '7' = 'Immune', 
+                     '8' = 'Immune', 
+                     '9' = 'NPC-like')
+
+data[["group"]] <- Idents(data)
+
+# DotPlot markers
+DotPlot(data, cols = c('lightgrey', 'red'),features = gene_list, assay = 'SCT', scale = TRUE) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text=element_text(size = 8))
+ggsave(file.path(plot_dir, "1_DotPlot_0010553-Region_4_EP41.pdf"), width=13, height=4)
+
+# Export metadata with cell names and new annotation
+cell_id <- data@meta.data %>% select(group)
+cell_id <-  cell_id %>%
+  rownames_to_column(var = "cell_id")
+
+write_csv(cell_id, file.path(data_dir, "0010553-Region_4_EP41_cell_id.csv"))
+
+
+
 
 
 
